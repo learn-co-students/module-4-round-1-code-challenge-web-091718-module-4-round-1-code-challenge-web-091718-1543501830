982bfc4e9358111b34d2573baf252dc1cca57646
jtang93 <tang.jonathan93@gmail.com>
2018-11-29 10:46:43 -0500

    checkpoint

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -4,12 +4,15 @@ import BotCard from "../components/BotCard";
 class BotCollection extends React.Component {
   //your code here
 
+  renderBotCards = () => {
+    return this.props.botsData.map(bot => <BotCard key={bot.id} bot={bot} setBotArmy={this.props.setBotArmy}/>)
+  }
+
   render(){
   	return (
-  	  <div className="ui four column grid">
+  	  <div className="ui four column grid" id="botCollection">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+          {this.renderBotCards()}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,51 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
+
+const botsURL = 'https://bot-battler-api.herokuapp.com/api/v1/bots'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  state = {
+    botsData: [],
+    botArmy: []
+  }
+
+ componentDidMount() {
+   fetch(botsURL)
+   .then(response => response.json())
+   .then(parsedData => this.setState({botsData: parsedData}))
+ }
+
+ setBotArmy = (botObj) => {
+   let army = this.state.botArmy
+   if (!army.includes(botObj)) {
+     this.setState({botArmy: [...army, botObj]})
+   }
+ }
+
+ delistBotArmy = (botObj) => {
+   let army = this.state.botArmy
+   let botIndex = army.findIndex(bot => bot === botObj)
+   army.splice(botIndex, 1)
+   this.setState({botArmy: army})
+ }
+
   render() {
     return (
       <div>
-        {/* put your components here */}
+
+        <YourBotArmy
+          botArmy={this.state.botArmy}
+          setBotArmy={this.delistBotArmy}
+        />
+
+        <BotCollection
+          botsData={this.state.botsData}
+          setBotArmy={this.setBotArmy}
+        />
+
       </div>
     );
   }
