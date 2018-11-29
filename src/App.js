5ed9f5bccf8d3b462c88dbd50820af04f1f66348
asc5025 <albert.s.chun@gmail.com>
2018-11-29 11:06:46 -0500

    partial

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import BotsPage from "./containers/BotsPage";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    bots: []
+  }
+
+  componentDidMount() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+      .then(response => response.json())
+      .then(botData => this.setState({ bots: botData }));
+    }
+
   render() {
     return (
       <div className="App">
-        <BotsPage />
+        <BotsPage bots={this.state.bots} />
       </div>
     );
   }
--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -2,14 +2,20 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
-  //your code here
+
+  renderCollection() {
+    return this.props.bots.map(bot => {
+      return (
+        <BotCard key={bot.id} bot={bot} handleClick={this.props.handleClick}/>
+      )
+    })
+  }
 
   render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+          {this.renderCollection()}
     		</div>
   	  </div>
   	);
