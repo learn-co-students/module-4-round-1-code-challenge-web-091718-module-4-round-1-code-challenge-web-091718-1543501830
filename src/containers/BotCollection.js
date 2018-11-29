f9ef94666c23d19c3828df64ead7953a26d8e84c
Trinacria <xcstolfax@gmail.comgit config --global user.name Christopher Stolfagit config --global user.email xcstolfax@gmail.com>
2018-11-29 10:27:02 -0500

    added add bot feature

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -3,13 +3,15 @@ import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
   //your code here
+  setBotCards = () => {
+    return this.props.robots.map(robot=> <BotCard recruitBot={this.props.recruitBot} key={robot.id} robot={robot} />)
+  }
 
   render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+          {this.setBotCards()}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,41 @@
 import React from "react";
+import BotCollection from "./BotCollection"
+import YourBotArmy from "./YourBotArmy"
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  state = {
+    robots: [],
+  }
+
+  componentDidMount(){
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(response => response.json())
+    .then(robots => this.setState({
+      robots: robots.map(robot=>{
+        robot.recruited = false
+        return robot
+      })
+    }))
+  }
+
+  recruitBot = (currentId) => {
+    this.setState({robots: this.state.robots.map(robot=>{
+        if (currentId === robot.id){
+          robot.recruited = !robot.recruited
+          return robot
+        } else {
+          return robot
+        }
+      })
+    })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy robots={this.state.robots} />
+        <BotCollection recruitBot={this.recruitBot} robots={this.state.robots} />
       </div>
     );
   }
